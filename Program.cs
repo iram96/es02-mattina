f73315b771ae5b0e5a124be9e7d04393ae2c5e03
iram96 <mariano.maselli@virgilio.it>
2022-05-11 11:47:35 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SecondoGiorno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int iSomma = esercizi_es02.StampaSomma(10, 20);
+
+            Console.WriteLine("La somma è {0}", iSomma);
+
+            int iNumeroDaRaddoppiare = 9;
+            esercizi_es02.RaddoppiaNumero(ref iNumeroDaRaddoppiare);
+            Console.WriteLine("il numero vale {0}", iNumeroDaRaddoppiare);
+
+            string sNome = "ciao";
+            //con ref passo una variabile, senza passo il valore della variabile
+            esercizi_es02.ModificaStringa(ref sNome);
+            Console.WriteLine("La parola è {0}", sNome);
+            */
+        }
+    }
+}
---
+++ esercizi.cs
@@ -0,0 +1,22 @@
+﻿namespace SecondoGiorno
+{
+    public class esercizi_es02
+    {
+        public static int StampaSomma(int Numero1, int Numero2)
+        {
+            int iSomma = Numero1 + Numero2;
+            return iSomma;
+        }
+        public static void RaddoppiaNumero(ref int iNumeroDaRaddoppiare)
+        {
+            iNumeroDaRaddoppiare = iNumeroDaRaddoppiare * 2;
+            //return con void non ho bisogno del return, al massimo return vuoto
+        }
+
+        public static void ModificaStringa(ref string sStringaDaModificare)
+        {
+            sStringaDaModificare = sStringaDaModificare + "abc";
+
+        }
+    }
+}
